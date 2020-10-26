98c05307b86dca1eb2d1c20afd306f3c4f05a3f4
etstringy <eddyteddyt@gmail.com>
2020-10-26 11:07:52 +0000

    Remove previous code ready for new design

--- src/index.js
+++ src/index.js
@@ -3,21 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
-
-//Dark theme
-const darkTheme = createMuiTheme({
-  palette: {
-    type: 'light',
-  },
-});
-
 ReactDOM.render(
-  <ThemeProvider theme={darkTheme}>
     <BrowserRouter>
       <Routes />
-    </BrowserRouter>
-  </ThemeProvider>,
+    </BrowserRouter>,
   document.getElementById('disq')
 );
--- src/routes.js
+++ src/routes.js
@@ -2,22 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Pages
-import DisqIndex from "./pages/landing";
-import DisqDashboard from "./pages/dashboard";
-import DisqDashboardUpload from "./pages/dashboard/upload";
-import DisqDashboardFiles from "./pages/dashboard/files";
-import DisqDashboardShorts from "./pages/dashboard/shorts";
+// import DisqIndex from "./pages/landing";
 
 function routes() {
   return (
     <main>
         <Switch>
-          <Route path='/' component={DisqIndex} exact />
-          <Route path='/dashboard' component={DisqDashboard} exact />
-          <Route path='/dashboard/upload' component={DisqDashboardUpload} exact />
-          <Route path='/dashboard/files' component={DisqDashboardFiles} exact />
-          <Route path='/dashboard/shorts' component={DisqDashboardShorts} exact />
-          {/* <Route component={Disq404} /> */}
+          {/* <Route path='/' component={DisqIndex} exact /> */}
         </Switch>
     </main>
   );
