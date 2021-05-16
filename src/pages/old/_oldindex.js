32577eddece79e8dc74b3b9203b7760fe6d0d089
etstringy <eddyteddyt@gmail.com>
2021-05-16 03:01:35 +0100

    path fix and ci fix

--- src/pages/old/_oldindex.js
+++ src/pages/old/_oldindex.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Head from "../components/Head";
+import Head from "../../components/Head";
 import Image from "next/image";
 import Twemoji from "react-twemoji";
 import check_token from "../components/TokenChecker";
 
-import config from "../config.json";
+import config from "../../config.json";
 
 class index extends React.Component {
   constructor(props) {
