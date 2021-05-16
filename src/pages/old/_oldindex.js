68e28c9dd350b02efe77cf4611ff70d444d5547e
etstringy <eddyteddyt@gmail.com>
2021-05-16 03:02:36 +0100

    ONE MORE path fix

--- src/pages/old/_oldindex.js
+++ src/pages/old/_oldindex.js
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "../../components/Head";
 import Image from "next/image";
 import Twemoji from "react-twemoji";
-import check_token from "../components/TokenChecker";
+import check_token from "../../components/TokenChecker";
 
 import config from "../../config.json";
 
