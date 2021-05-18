63216589e618cefffc3412b7bdf76621600e09a9
etstringy <eddyteddyt@gmail.com>
2021-05-19 00:12:58 +0100

    Add new favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,6 +24,15 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2"/>
+          <link rel="manifest" href="/site.webmanifest?v=2"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#6812ca"/>
+          <link rel="shortcut icon" href="/favicon.ico?v=2"/>
+          <meta name="apple-mobile-web-app-title" content="Disq"/>
+          <meta name="application-name" content="Disq"/>
+          <meta name="msapplication-TileColor" content="#7f0086"></meta>
         </Head>
         <body>
           <Main />
